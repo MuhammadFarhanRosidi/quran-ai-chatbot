21cff344064abefd401f9751822f541666ce5349
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-07 17:18:35 +0700

    feat: handle join button card

---
+++ client/src/components/Card.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import instance from "../config/axiosInstance";
+
+export default function Card({ courses }) {
+  const navigte = useNavigate();
+  async function handlerJoin(id) {
+    try {
+      const { data } = await instance({
+        url: `/joinCourse/${id}`,
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.access_token}`,
+        },
+      });
+      console.log(data, "<<<<");
+      navigte("/myCourses");
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  return (
+    <div className="card bg-base-100 w-80 shadow-xl pt-5">
+      <div className="card-body">
+        <h2 className="card-title">
+          {courses.nama_latin} - ({courses.nomor})
+        </h2>
+        <p>{courses.arti}</p>
+        <div className="card-actions">
+          <button
+            onClick={() => handlerJoin(courses.nomor)}
+            className="btn bg-green-800 text-white"
+          >
+            Add to my course
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
