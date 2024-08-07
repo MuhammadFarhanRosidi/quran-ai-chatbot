9dce834180a28dc52050608e3ea11346e164e8a0
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-07 17:20:11 +0700

    fix: edit props card

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import instance from "../config/axiosInstance";
+import Card from "../components/Card";
+
 export default function Courses() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [courses, setCourses] = useState([]);
+  async function showCourses() {
+    try {
+      const { data } = await instance({
+        url: "/courses",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.access_token}`,
+        },
+      });
+      setCourses(data);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  useEffect(() => {
+    showCourses();
+  }, []);
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center gap-9">
+        {courses.map((e) => (
+          <Card key={e.nomor} courses={e} />
+        ))}
+      </div>
+    </div>
+  );
 }
