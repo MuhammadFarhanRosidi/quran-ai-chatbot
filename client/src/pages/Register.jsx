3238415cd8aceb2dea8d705703a137b3932f6c26
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-07 10:44:45 +0700

    feat: create register page and all routings

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,86 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import instance from "../config/axiosInstance";
+
 export default function Register() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  async function handlerRegister(e) {
+    try {
+      e.preventDefault();
+      const { data } = await instance({
+        url: "/register",
+        method: "POST",
+        data: {
+          username,
+          email,
+          password,
+        },
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body" onSubmit={handlerRegister}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Username</span>
+              </label>
+              <input
+                type="text"
+                placeholder="username"
+                className="input input-bordered"
+                required
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button type="submit" className="btn btn-primary">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/routes/router.jsx
+++ client/src/routes/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, redirect } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import MainLayout from "../pages/MainLayout";
 import Courses from "../pages/Courses";
 import MyCourses from "../pages/MyCourses";
 import DetailCourse from "../pages/DetailCourse";
@@ -9,14 +10,76 @@ import EditCourse from "../pages/EditCourse";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <Home />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+    loader: () => {
+      if (localStorage.access_token) {
+        redirect("/");
+      }
+      return null;
+    },
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    loader: () => {
+      if (localStorage.access_token) {
+        redirect("/");
+      }
+      return null;
+    },
+  },
+  {
+    path: "/",
+    element: <MainLayout />,
     // loader: () => {
-    //   if (localStorage.access_token) {
-    //     return redirect("/");
+    //   if (!localStorage.access_token) {
+    //     redirect("/login");
     //   }
     //   return null;
     // },
+    children: [
+      {
+        path: "",
+        element: <Courses />,
+        // loader: () => {
+        //   if (!localStorage.access_token) {
+        //   }
+        //   redirect("/login");
+        // },
+      },
+      {
+        path: "myCourses",
+        element: <MyCourses />,
+        // loader: () => {
+        //   if (!localStorage.access_token) {
+        //   }
+        //   redirect("/login");
+        // },
+      },
+      {
+        path: "detailCourse",
+        element: <DetailCourse />,
+        // loader: () => {
+        //   if (!localStorage.access_token) {
+        //   }
+        //   redirect("/login");
+        // },
+      },
+      {
+        path: "editCourse",
+        element: <EditCourse />,
+        // loader: () => {
+        //   if (!localStorage.access_token) {
+        //   }
+        //   redirect("/login");
+        // },
+      },
+    ],
   },
 ]);
 
