a0111982f2eb11890d8b22549e8b54c2b179bb58
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-08 12:26:02 +0700

    fix: fix detailcourses and fix mycourses

--- client/src/pages/DetailCourse.jsx
+++ client/src/pages/DetailCourse.jsx
@@ -1,37 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import instance from "../config/axiosInstance";
+
 export default function DetailCourse() {
+  const [chapter, setChapter] = useState({});
+  const { id } = useParams();
+  async function showDetailCourse() {
+    try {
+      const { data } = await instance({
+        url: `/detailCourse/${id}`,
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.access_token}`,
+        },
+      });
+      setChapter(data);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  }
+  useEffect(() => {
+    showDetailCourse();
+  }, []);
   return (
     <>
-      <h1 className="text-center text-5xl pt-8">Judul Materi</h1>
-      <p className="text-center">Kategori</p>
+      <h1 className="text-center text-5xl pt-8">{chapter.nama}</h1>
+      <p className="text-center mt-2">{chapter.arti}</p>
       <div className="card bg-base-100 w-4/5 shadow-xl mx-40">
         <div className="card-body shadow-lg shadow-green-500/50">
-          <h2>Sub Judul</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
-            alias? Accusantium totam rem obcaecati necessitatibus! Ducimus dolor
-            porro commodi molestias minima accusamus soluta neque sed ab,
-            repudiandae, cumque rerum error?
-          </p>
-          <h2>Sub Judul</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
-            alias? Accusantium totam rem obcaecati necessitatibus! Ducimus dolor
-            porro commodi molestias minima accusamus soluta neque sed ab,
-            repudiandae, cumque rerum error?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
-            alias? Accusantium totam rem obcaecati necessitatibus! Ducimus dolor
-            porro commodi molestias minima accusamus soluta neque sed ab,
-            repudiandae, cumque rerum error?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
-            alias? Accusantium totam rem obcaecati necessitatibus! Ducimus dolor
-            porro commodi molestias minima accusamus soluta neque sed ab,
-            repudiandae, cumque rerum error?
-          </p>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          {chapter.ayat.map((e) => {
+            return (
+              <div key={e.id} className="p-5 rounded-xl border ">
+                <div dir="rtl" className="flex">
+                  <button className="ml-4 w-12 h-12 text-white bg-green-600 rounded-2xl">
+                    {e.nomor}
+                  </button>
+                  <h3 dir="rtl">{e.ar}</h3>
+                </div>
+
+                <p>{e.tr}</p>
+                <p>{e.idn}</p>
+              </div>
+            );
+          })}
           <div className="card-actions justify-end">
             <button className="btn btn-secondary">Like</button>
             <button className="btn btn-accent">Download</button>
--- client/src/pages/MyCourses.jsx
+++ client/src/pages/MyCourses.jsx
@@ -1,76 +1,72 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchMyCourses, removeCourse } from "../store/slices/coursesSlice";
-import instance from "../config/axiosInstance";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import instance from "../config/axiosInstance";
 
 export default function MyCourses() {
-  const dispatch = useDispatch();
-  const myCourses = useSelector((state) => state.courses.myCourses);
-  const status = useSelector((state) => state.courses.status);
-  const error = useSelector((state) => state.courses.error);
-
-  useEffect(() => {
-    if (status === "idle") {
-      dispatch(fetchMyCourses());
-    }
-  }, [status, dispatch]);
-
-  const handleLeave = async (id) => {
+  const [myCourses, setMyCourses] = useState([]);
+  async function handleShowMyCourse() {
     try {
-      await instance({
-        url: `/deleteMyCourse/${id}`,
-        method: "DELETE",
+      const { data } = await instance({
+        url: "/my-courses",
+        method: "GET",
         headers: {
           Authorization: `Bearer ${localStorage.access_token}`,
         },
       });
-      dispatch(removeCourse(id));
+      setMyCourses(data.Courses);
     } catch (error) {
       console.log(error.response ? error.response.data.message : error.message);
     }
-  };
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
   }
-
-  if (status === "failed") {
-    return <div>Error: {error}</div>;
+  useEffect(() => {
+    handleShowMyCourse();
+  }, []);
+  async function handleLeave(id) {
+    const { data } = await instance({
+      url: `/deleteMyCourse/${id}`,
+      method: "Delete",
+      headers: {
+        Authorization: `Bearer ${localStorage.access_token}`,
+      },
+    });
+    handleShowMyCourse();
   }
-
   return (
-    <div className="flex flex-wrap gap-9 justify-center">
-      {myCourses.map((e) => (
-        <div key={e.id} className="card bg-base-100 w-80 shadow-xl pt-5">
-          <div key={e.chapterId} className="card-body">
-            <h2 className="card-title">
-              {e.title} - {e.chapterId}
-            </h2>
-            <p>{e.description}</p>
-            <div className="card-actions">
-              <button
-                onClick={() => {}}
-                className="btn bg-green-800 text-white"
-              >
-                See detail
-              </button>
-              <Link
-                to={`/editCourse/${e.id}`}
-                className="btn bg-cyan-400 text-white"
-              >
-                Edit
-              </Link>
-              <button
-                onClick={() => handleLeave(e.id)}
-                className="btn bg-red-700 text-white"
-              >
-                Leave
-              </button>
+    <>
+      <div className="flex flex-wrap gap-9 justify-center">
+        {myCourses.map((e) => {
+          return (
+            <div key={e.id} className="card bg-base-100 w-80 shadow-xl pt-5">
+              <div key={e.chapterId} className="card-body">
+                <h2 className="card-title">
+                  {e.title} - {e.chapterId}
+                </h2>
+                <p>{e.description}</p>
+                <div className="card-actions">
+                  <Link
+                    to={`/detailCourse/${e.chapterId}`}
+                    className="btn bg-green-800 text-white"
+                  >
+                    See detail
+                  </Link>
+                  <Link
+                    to={`/editCourse/${e.id}`}
+                    className="btn bg-cyan-400 text-white"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    onClick={() => handleLeave(e.id)}
+                    className="btn bg-red-700 text-white"
+                  >
+                    Leave
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
